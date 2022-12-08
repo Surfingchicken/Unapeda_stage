b7df1a4ad42a5e9ebe9f5d125a3a286e7f9a9937
Alexandre Delfino <alex.delf@laposte.net>
2022-12-08 09:57:54 +0100

    overall 1

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 document.addEventListener("DOMContentLoaded", e => {
     e.preventDefault;
+    let i;
+    for (i=1; i<=2; i++){
+        let btn = "btn_"+i;
+        let ul = "ul_"+i;
+        document.getElementById(btn).addEventListener("click", ()=>{
+            var x = document.getElementById(ul);
+            if (getComputedStyle(x).display == "none") {
+                x.style.display = "block";
+                document.getElementById(btn).style.transform="none";
+            } 
+            else {
+            x.style.display = "none";
+            document.getElementById(btn).style.transform="rotate(-90deg)";
+            }
+        });
+    }
+
     const items = document.querySelectorAll('section.news');
     const nbSlide = items.length;
     const suivant = document.querySelector('.right');
